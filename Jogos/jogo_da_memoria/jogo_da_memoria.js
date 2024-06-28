b68a9889e71ef923c42a3b17e3603bcd0bf2d908
ARTHUR ARI LEMKE <arthur_lemke@estudante.sesisenai.org.br>
2024-06-28 11:38:06 -0300

    Jogo da memoria funcionando

--- Jogos/jogo_da_memoria/jogo_da_memoria.js
+++ Jogos/jogo_da_memoria/jogo_da_memoria.js
@@ -1,6 +1,6 @@
 const cartas_container = document.getElementById('cartas-container');
 
-const cartas = {
+const cartas_arquivos = {
     'Cone de trânsito'          : 'cartas/cone_de_transito.svg',
     'Faixa de segurança'        : 'cartas/faixa_de_seguranca.svg',
     'Limite de velocidade'      : 'cartas/limite_de_velocidade.svg',
@@ -13,17 +13,15 @@ const cartas = {
     'Trilho de trem'            : 'cartas/trilho_de_trem.svg'
 }
 
-var cartas_visuais = new Array();
-var cartas_descritivas = new Array();
+var cartas_objetos = new Array();
+var pontos = 0;
 
-var amostragem_de_cartas = new Array();
-
-Object.keys(cartas).forEach(key => {
+Object.keys(cartas_arquivos).forEach(key => {
     //Para as cartas com Imagems
     var div = document.createElement("div");
     div.classList = "carta ";
     var img = document.createElement("img");
-    img.src = cartas[key];
+    img.src = cartas_arquivos[key];
     div.classList += key.replace(/\s+/g, '-').toLowerCase();
     div.appendChild(img);
     
@@ -35,43 +33,107 @@ Object.keys(cartas).forEach(key => {
     div.classList, div2.classList += key.replace(/\s+/g, '-').toLowerCase();
     div2.appendChild(p);
     
-    div.addEventListener('click', (event) => girarCarta(event));
-    div2.addEventListener('click', (event) => girarCarta(event));
+    div.addEventListener('click', (event) => girarCarta(event.target));
+    div2.addEventListener('click', (event) => girarCarta(event.target));
     
-    cartas_visuais.push(div);
-    cartas_descritivas.push(div2);
+    cartas_objetos.push(div);
+    cartas_objetos.push(div2);
 
-    console.log(key, cartas[key]);
+    console.log(key, cartas_arquivos[key]);
 });
 
-cartas_visuais.forEach(element => {
-    cartas_container.append(element);
-});
+Array.prototype.shuffle = function() {
+    let indice = this.length;
+    
+    while(indice) {
+        const indiceAleatorio = Math.floor(Math.random() * indice--);
+        [this[indice], this[indiceAleatorio]] = 
+            [this[indiceAleatorio], this[indice]];
+    }
 
-cartas_descritivas.forEach(element => {
-    cartas_container.append(element);
-});
+    return this;
+}
 
-function girarCarta(event){
-    var carta_exibida = !(event.target.children[0].style.display == 'none');
+document.getElementById("reiniciar").addEventListener('click', () => embaralhar());
+function embaralhar() {
+    cartas_objetos.shuffle();
+    
+    cartas_objetos.forEach(element => {
+        element.classList.remove('carta-baixo');
+        element.classList.remove('carta-cima');
+        element.children[0].style.display = 'block';
+    });
+
+    atualizar();
+};
+
+function atualizar() {
+    cartas_objetos.forEach(element => {
+        cartas_container.append(element);
+    });
+}
+embaralhar();
+
+function iniciar() {
+    console.log("Iniciar");
+    cartas_objetos.forEach(element => ocultarCarta(element));
+}
+
+document.getElementById("iniciar").addEventListener('click', () => iniciar());
+
+function girarCarta(element){
+    if (selecionado.length > 1)
+        return;
+
+    qtd_selecionado++;
+    var carta_exibida = !(element.children[0].style.display == 'none');
     console.log(carta_exibida);
 
+    selecionado.push(element)
+
     if (carta_exibida)
-        ocultarCarta(event);
+        ocultarCarta(element);
     else
-        mostrarCarta(event);
+        mostrarCarta(element);
+
+    if (qtd_selecionado > 1)
+        compararSelecionado();
 }
 
-function ocultarCarta(event){
-    event.target.classList.remove('carta-cima');
-    event.target.classList.add('carta-baixo');
+function ocultarCarta(element){
+    element.classList.remove('carta-cima');
+    element.classList.add('carta-baixo');
 
-    event.target.children[0].style.display = 'none';
+    element.children[0].style.display = 'none';
 }
 
-function mostrarCarta(event){
-    event.target.classList.remove('carta-baixo');
-    event.target.classList.add('carta-cima');
+function mostrarCarta(element){
+    element.classList.remove('carta-baixo');
+    element.classList.add('carta-cima');
 
-    event.target.children[0].style.display = 'block';
+    element.children[0].style.display = 'block';
+}
+
+var qtd_selecionado = 0;
+var selecionado = new Array();
+
+function compararSelecionado(){
+    console.log("Comparando");
+    console.log(selecionado);
+    var igual = selecionado[0].classList[1] == selecionado[1].classList[1];
+
+    if (igual){
+        pontos++;
+        selecionado =  new Array();
+    }
+    else{
+        setTimeout(restaurarSelecao, 1000);
+    }
+
+    qtd_selecionado = 0;
+}
+
+function restaurarSelecao(){
+    selecionado.forEach(element => ocultarCarta(element));
+    selecionado = new Array();
 }
